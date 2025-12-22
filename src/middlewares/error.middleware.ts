f2937124f11432fc9b214a8f317381e585e038fb
cyxabima <ukashaanwerali@gmail.com>
2025-12-22 18:18:49 +0500

    error handling

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -8,12 +8,21 @@ const errorHandler = (err: ApiError, req: Request, res: Response, next: NextFunc
     const message = err.message || "Internal Server Error";
     const errorType = err.errorType || "Unknown Error";
 
-    res.status(statusCode).json({
-        success: false,
-        message,
-        errorType
-    });
-    next()
+    if (statusCode < 500) {
+        res.status(statusCode).json({
+            success: false,
+            message,
+            errorType
+        });
+    }
+    else {
+        res.status(statusCode).json({
+            success: false,
+            message: "something went wrong",
+            errorType: "internal server error"
+        });
+    }
+    next();
 };
 
 export { errorHandler, ApiError };
